65b3f79f71351c2b368b9315a13b53c4e22b297a
exp008 <154838747+exp008@users.noreply.github.com>
2024-10-16 12:17:04 +0300

    Delete Lab1 directory

--- Lab1/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', index),
-    path('participants/<int:participant_id>/', participant),
-    path('tenders/<int:tender_id>/', tender),
-]
